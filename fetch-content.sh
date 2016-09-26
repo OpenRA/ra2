ad3075002934ef0ae3e31e8c6f5f95b90ecf5eb8
Youri van Mill <yourivanmill@gmail.com>
2016-09-26 17:38:56 +0200

    Fetch-content: Check for OS and change the download directory.

--- fetch-content.sh
+++ fetch-content.sh
@@ -3,10 +3,18 @@
 # Die on any error for Travis CI to automatically retry:
 set -e
 
-if [ ! -e ~/.openra/Content/ra2/ra2.mix ]; then
+if [ "$(uname)" == "Darwin" ]; then
+	DIR="$HOME/Library/Application Support/OpenRA/Content/ra2/"
+else
+	DIR="$HOME/.openra/Content/ra2/"
+fi
+
+if [ ! -e "$DIR" ]; then
 	echo "Downloading RA2 mod content"
-	mkdir -p ~/.openra/Content/ra2/
-	pushd ~/.openra/Content/ra2/
+
+	mkdir -p "$DIR"
+	pushd "$DIR"
+
 	wget http://xwis.net/downloads/Red-Alert-2-Multiplayer.exe
 	7z e Red-Alert-2-Multiplayer.exe
 	rm *.exe *.dll *.DLL *.wav *.mmp *.CFG *.WAR *.cache
