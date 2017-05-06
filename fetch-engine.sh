506e6b55c4d9444ce2540fd70a370e815bf025b5
abcdefg30 <alphabet@streber24.de>
2017-05-06 22:14:35 +0200

    Add the new launch scripts

---
+++ fetch-engine.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+# Helper script used to check and update engine dependencies
+# This should not be called manually
+
+command -v curl >/dev/null 2>&1 || { echo >&2 "The OpenRA mod template requires curl."; exit 1; }
+command -v python >/dev/null 2>&1 || { echo >&2 "The OpenRA mod template requires python."; exit 1; }
+
+TEMPLATE_LAUNCHER=$(python -c "import os; print(os.path.realpath('$0'))")
+TEMPLATE_ROOT=$(dirname "${TEMPLATE_LAUNCHER}")
+
+# shellcheck source=mod.config
+. "${TEMPLATE_ROOT}/mod.config"
+
+CURRENT_ENGINE_VERSION=$(cat "${ENGINE_DIRECTORY}/VERSION" 2> /dev/null)
+
+if [ -f "${ENGINE_DIRECTORY}/VERSION" ] && [ "${CURRENT_ENGINE_VERSION}" = "${ENGINE_VERSION}" ]; then
+	exit 0
+fi
+
+if [ "${AUTOMATIC_ENGINE_MANAGEMENT}" = "True" ]; then
+	echo "OpenRA engine version ${ENGINE_VERSION} is required."
+
+	if [ -d "${ENGINE_DIRECTORY}" ]; then
+		if [ "${CURRENT_ENGINE_VERSION}" != "" ]; then
+			echo "Deleting engine version ${CURRENT_ENGINE_VERSION}."
+		else
+			echo "Deleting existing engine (unknown version)."
+		fi
+
+		rm -rf "${ENGINE_DIRECTORY}"
+	fi
+
+	echo "Downloading engine..."
+	curl -s -L -o "${AUTOMATIC_ENGINE_TEMP_ARCHIVE_NAME}" -O "${AUTOMATIC_ENGINE_SOURCE}" || exit 3
+
+	# Github zipballs package code with a top level directory named based on the refspec
+	# Extract to a temporary directory and then move the subdir to our target location
+	REFNAME=$(unzip -qql "${AUTOMATIC_ENGINE_TEMP_ARCHIVE_NAME}" | head -n1 | tr -s ' ' | cut -d' ' -f5-)
+
+	rm -rf "${AUTOMATIC_ENGINE_EXTRACT_DIRECTORY}"
+	mkdir "${AUTOMATIC_ENGINE_EXTRACT_DIRECTORY}"
+	unzip -qq -d "${AUTOMATIC_ENGINE_EXTRACT_DIRECTORY}" "${AUTOMATIC_ENGINE_TEMP_ARCHIVE_NAME}"
+	mv "${AUTOMATIC_ENGINE_EXTRACT_DIRECTORY}/${REFNAME}" "${ENGINE_DIRECTORY}"
+	rmdir "${AUTOMATIC_ENGINE_EXTRACT_DIRECTORY}"
+	rm "${AUTOMATIC_ENGINE_TEMP_ARCHIVE_NAME}"
+
+	echo "Compiling engine..."
+	cd "${ENGINE_DIRECTORY}" || exit 1
+	make version VERSION="${ENGINE_VERSION}"
+	exit 0
+fi
+
+echo "Automatic engine management is disabled."
+echo "Please manually update the engine to version ${ENGINE_VERSION}."
+exit 1
+
---
+++ launch-dedicated.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# Usage:
+#  $ ./launch-dedicated.sh # Launch a dedicated server with default settings
+#  Read the file to see which settings you can override
+
+set -e
+command -v make >/dev/null 2>&1 || { echo >&2 "This script requires make."; exit 1; }
+command -v python >/dev/null 2>&1 || { echo >&2 "This script requires python."; exit 1; }
+command -v mono >/dev/null 2>&1 || { echo >&2 "This script requires mono."; exit 1; }
+
+TEMPLATE_LAUNCHER=$(python -c "import os; print(os.path.realpath('$0'))")
+TEMPLATE_ROOT=$(dirname "${TEMPLATE_LAUNCHER}")
+
+# shellcheck source=mod.config
+. "${TEMPLATE_ROOT}/mod.config"
+
+if [ -f "${TEMPLATE_ROOT}/user.config" ]; then
+	# shellcheck source=user.config
+	. "${TEMPLATE_ROOT}/user.config"
+fi
+
+MOD_SEARCH_PATHS="${TEMPLATE_ROOT}/mods"
+if [ "${INCLUDE_DEFAULT_MODS}" = "True" ]; then
+	MOD_SEARCH_PATHS="${MOD_SEARCH_PATHS},./mods"
+fi
+
+NAME="${Name:-"Dedicated Server"}"
+LAUNCH_MOD="${Mod:-"${MOD_ID}"}"
+LISTEN_PORT="${ListenPort:-"1234"}"
+EXTERNAL_PORT="${ExternalPort:-"1234"}"
+ADVERTISE_ONLINE="${AdvertiseOnline:-"True"}"
+ENABLE_SINGLE_PLAYER="${EnableSingleplayer:-"False"}"
+PASSWORD="${Password:-""}"
+
+cd "${TEMPLATE_ROOT}"
+if [ ! -f "${ENGINE_DIRECTORY}/OpenRA.Game.exe" ]; then
+	echo "Required engine files not found."
+	echo "Run \`make\` in the mod directory to fetch and build the required files, then try again.";
+	exit 1
+fi
+
+cd "${ENGINE_DIRECTORY}"
+
+while true; do
+     MOD_SEARCH_PATHS="${MOD_SEARCH_PATHS}" mono --debug OpenRA.Server.exe Game.Mod="${LAUNCH_MOD}" \
+     Server.Name="${NAME}" Server.ListenPort="${LISTEN_PORT}" Server.ExternalPort="${EXTERNAL_PORT}" \
+     Server.AdvertiseOnline="${ADVERTISE_ONLINE}" \
+     Server.EnableSingleplayer="${ENABLE_SINGLE_PLAYER}" Server.Password="${PASSWORD}"
+done
