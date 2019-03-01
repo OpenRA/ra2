a037cafd2a154bd7636b30db2bad48e1dac438d1
abcdefg30 <alphabet@streber24.de>
2019-03-01 10:51:16 +0100

    Fix missing explicit interface implementations

--- OpenRA.Mods.RA2/Traits/Render/WithMindControlArc.cs
+++ OpenRA.Mods.RA2/Traits/Render/WithMindControlArc.cs
@@ -58,7 +58,7 @@ namespace OpenRA.Mods.RA2.Traits
 			this.info = info;
 		}
 
-		public void Created(Actor self)
+		void INotifyCreated.Created(Actor self)
 		{
 			mindController = self.TraitOrDefault<MindController>();
 			mindControllable = self.TraitOrDefault<MindControllable>();
